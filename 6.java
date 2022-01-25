dc085806feb1821c523a98428b8731dc01c74f3e
cw0714 <32086660+cw0714@users.noreply.github.com>
2022-01-25 23:09:01 +0900

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(a + " * " + i +" = " + a * i);
+
+
+        }
