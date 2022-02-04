e7a71db9db4dc681bd7b70b563ebde6c44427a76
cw0714 <32086660+cw0714@users.noreply.github.com>
2022-02-04 14:07:10 +0900

    Create 13.java
    11022

---
+++ 13.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(in);
+        int t = sc.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+        }
+
+
+    }
+}
